b642d71fd3657aa275792b70205758b053089bed
sage1991 <free_eddy@naver.com>
2020-03-19 01:44:38 +0900

    [ADD] 1. typescript 적용중...

---
+++ src/Application.ts
@@ -0,0 +1,22 @@
+class Application {
+    
+    // device ready event
+    public onReady() {
+
+    }
+
+    // android back key event
+    public onBackKeyTouched() {
+
+    }
+
+    // device pause event
+    public onPause() {
+
+    }
+
+    // device resume event
+    public onResume() {
+
+    }
+}
